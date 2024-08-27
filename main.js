60b5145b3b28c238bb558cc5974152eb9dfafd80
Anastasiia-Rechevska <volokh.qs@gmail.com>
2024-08-27 17:15:40 +0300

    EDT-412/Fead:made changes to the code after code review

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
-const items = Array.from(document.querySelectorAll(".accordion-item"));
+(function() {
+const items = (document.querySelectorAll(".accordion-item"));
+if (!items || items.length === 0) return null;
 
 items.forEach((accordionItem) => {
+    const accordionHeader = accordionItem.querySelector(".accordion-header");
+    const accordionBody = accordionItem.querySelector(".accordion-body");
+    if (!accordionHeader || !accordionBody) return null;
+
     if (!accordionItem.classList.contains("active")) {
-        accordionItem.querySelector(".accordion-body").style.maxHeight = 0;
+        accordionBody.style.maxHeight = 0;
     }
-    accordionItem.querySelector(".accordion-header").addEventListener("click", accordionItemHandler);
+    accordionHeader.addEventListener("click", accordionItemHandler);
 });
 
 function accordionItemHandler(e) {
-    e.preventDefault();
-
     const currentAccordionItem = e.target.closest(".accordion-item");
     const currentAccordionBody = currentAccordionItem.querySelector(".accordion-body");
+    if (!currentAccordionItem || !currentAccordionBody) return null;
 
     if (currentAccordionItem.classList.contains("active")) {
         currentAccordionItem.classList.remove("active");
         currentAccordionBody.style.maxHeight = 0;
+
     } else {
         items.forEach((accordionItem) => {
-            accordionItem.classList.remove("active");
-            accordionItem.querySelector(".accordion-body").style.maxHeight = 0;
+            const accordionBody = accordionItem.querySelector(".accordion-body");
+            if (!accordionBody || !accordionItem) return null;
+            
+            if (accordionBody) accordionBody.style.maxHeight = 0;
+                accordionItem.classList.remove("active");
         });
         currentAccordionItem.classList.add("active");
         currentAccordionBody.style.maxHeight = currentAccordionBody.scrollHeight + "px";
     }
 }
+})();
 
 
 
